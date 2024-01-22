65a85e15b8d33567163383c0e97535cb47678103
UMR-scut <a1534337999@outlook.com>
2024-01-22 10:26:56 +0800

    lab4 finish

--- kern/pmap.c
+++ kern/pmap.c
@@ -213,7 +213,7 @@ mem_init(void)
 	//       overwrite memory.  Known as a "guard page".
 	//     Permissions: kernel RW, user NONE
 	// Your code goes here:
-    boot_map_region(kern_pgdir, KSTACKTOP - KSTKSIZE, KSTKSIZE, PADDR(bootstack), PTE_W);
+    //boot_map_region(kern_pgdir, KSTACKTOP - KSTKSIZE, KSTKSIZE, PADDR(bootstack), PTE_W);
 
 	//////////////////////////////////////////////////////////////////////
 	// Map all of physical memory at KERNBASE.
@@ -275,7 +275,8 @@ mem_init_mp(void)
 	//     Permissions: kernel RW, user NONE
 	//
 	// LAB 4: Your code here:
-
+    for(int i=0;i<NCPU;++i)
+    boot_map_region(kern_pgdir,KSTACKTOP - i * (KSTKSIZE + KSTKGAP)- KSTKSIZE,KSTKSIZE,PADDR(percpu_kstacks[i]),PTE_W);
 }
 
 // --------------------------------------------------------------
@@ -317,7 +318,7 @@ page_init(void)
 	size_t i;
 	page_free_list=NULL;
 	for (i = 0; i < npages; i++) {
-	        if(i==0)
+	        if(i==0||i==PGNUM(MPENTRY_PADDR))
 	        {
 	          pages[i].pp_ref = 1;
 	        }
@@ -603,7 +604,19 @@ mmio_map_region(physaddr_t pa, size_t size)
 	// Hint: The staff solution uses boot_map_region.
 	//
 	// Your code here:
-	panic("mmio_map_region not implemented");
+	physaddr_t begin = ROUNDDOWN(pa,PGSIZE);
+	physaddr_t end = ROUNDUP(pa+size,PGSIZE);
+    size = end-begin;
+
+	if(size>MMIOLIM-base) panic("mmio_map_region overflow MMIOLIM");
+    
+	boot_map_region(kern_pgdir,base,size,begin,PTE_PCD|PTE_PWT|PTE_W);
+
+	void *ret = (void *)base;
+	base+= size;
+
+	return ret;
+	
 }
 
 static uintptr_t user_mem_check_addr;
--- kern/trap.c
+++ kern/trap.c
@@ -85,7 +85,13 @@ static const char *trapname(int trapno)
     void simd_handler();
     void syscall_handler();
     void default_handler();
-	
+	void irq_timer_handler();
+	void irq_kbd_handler();
+	void irq_serial_handler();
+	void irq_spurious_handler();
+	void irq_ide_handler();
+	void irq_error_handler();
+
 void
 trap_init(void)
 {
@@ -112,6 +118,13 @@ trap_init(void)
     SETGATE(idt[T_SIMDERR],0,GD_KT,simd_handler,0);
     SETGATE(idt[T_SYSCALL],0,GD_KT,syscall_handler,3);
     SETGATE(idt[T_DEFAULT],0,GD_KT,default_handler,0);
+	SETGATE(idt[IRQ_OFFSET+IRQ_TIMER],0,GD_KT,irq_timer_handler,0);
+	SETGATE(idt[IRQ_OFFSET+IRQ_KBD],0,GD_KT,irq_kbd_handler,0);
+	SETGATE(idt[IRQ_OFFSET+IRQ_SERIAL],0,GD_KT,irq_serial_handler,0);
+	SETGATE(idt[IRQ_OFFSET+IRQ_SPURIOUS],0,GD_KT,irq_spurious_handler,0);
+	SETGATE(idt[IRQ_OFFSET+IRQ_IDE],0,GD_KT,irq_ide_handler,0);
+	SETGATE(idt[IRQ_OFFSET+IRQ_ERROR],0,GD_KT,irq_error_handler,0);
+
 
 	// Per-CPU setup 
 	trap_init_percpu();
@@ -145,21 +158,21 @@ trap_init_percpu(void)
 	// user space on that CPU.
 	//
 	// LAB 4: Your code here:
-
+    
 	// Setup a TSS so that we get the right stack
 	// when we trap to the kernel.
-	ts.ts_esp0 = KSTACKTOP;
-	ts.ts_ss0 = GD_KD;
-	ts.ts_iomb = sizeof(struct Taskstate);
+	thiscpu->cpu_ts.ts_esp0 = KSTACKTOP - thiscpu->cpu_id * (KSTKSIZE + KSTKGAP);
+	thiscpu->cpu_ts.ts_ss0 = GD_KD;
+	thiscpu->cpu_ts.ts_iomb = sizeof(struct Taskstate);
 
 	// Initialize the TSS slot of the gdt.
-	gdt[GD_TSS0 >> 3] = SEG16(STS_T32A, (uint32_t) (&ts),
+	gdt[(GD_TSS0 >> 3)+(thiscpu->cpu_id)] = SEG16(STS_T32A, (uint32_t) (&(thiscpu->cpu_ts)),
 					sizeof(struct Taskstate) - 1, 0);
-	gdt[GD_TSS0 >> 3].sd_s = 0;
+	gdt[(GD_TSS0 >> 3)+(thiscpu->cpu_id)].sd_s = 0;
 
 	// Load the TSS selector (like other segment selectors, the
 	// bottom three bits are special; we leave them 0)
-	ltr(GD_TSS0);
+	ltr(GD_TSS0+((thiscpu->cpu_id)<<3));
 
 	// Load the IDT
 	lidt(&idt_pd);
@@ -248,6 +261,10 @@ trap_dispatch(struct Trapframe *tf)
 				                    tf->tf_regs.reg_edi,
 				                    tf->tf_regs.reg_esi);
 		break;
+	case IRQ_OFFSET+IRQ_TIMER:
+	    lapic_eoi();
+		sched_yield();
+		break;
 	default:
 	
 
@@ -289,7 +306,7 @@ trap(struct Trapframe *tf)
 		// serious kernel work.
 		// LAB 4: Your code here.
 		assert(curenv);
-
+        lock_kernel();
 		// Garbage collect if current enviroment is a zombie
 		if (curenv->env_status == ENV_DYING) {
 			env_free(curenv);
@@ -369,6 +386,32 @@ page_fault_handler(struct Trapframe *tf)
 	//   (the 'tf' variable points at 'curenv->env_tf').
 
 	// LAB 4: Your code here.
+    if(curenv->env_pgfault_upcall)
+	{
+		uintptr_t stack_top = UXSTACKTOP;
+		if (UXSTACKTOP - PGSIZE < tf->tf_esp && tf->tf_esp < UXSTACKTOP) {
+			stack_top = tf->tf_esp;
+		}
+
+		uint32_t size = sizeof(struct UTrapframe) + sizeof(uint32_t);
+
+		user_mem_assert(curenv, (void *)stack_top - size, size, PTE_U | PTE_W);
+
+		struct UTrapframe *utr = (struct UTrapframe *)(stack_top - size);
+		utr->utf_fault_va = fault_va;
+		utr->utf_err = tf->tf_err;
+		utr->utf_regs = tf->tf_regs;
+		utr->utf_eip = tf->tf_eip;
+		utr->utf_eflags = tf->tf_eflags;
+		utr->utf_esp = tf->tf_esp;
+
+        tf->tf_eip = (uintptr_t)curenv->env_pgfault_upcall;
+        tf->tf_esp = (uintptr_t)utr;
+
+		env_run(curenv);
+
+	}
+
 
 	// Destroy the environment that caused the fault.
 	cprintf("[%08x] user fault va %08x ip %08x\n",
